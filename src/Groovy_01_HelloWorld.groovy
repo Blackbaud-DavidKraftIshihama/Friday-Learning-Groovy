5af7b97cdfd0bb021a581d32a49ec1d1e7c7ecf9
David Kraft-Ishihama <david.kraft-ishihama@blackbaud.com>
2014-10-28 09:23:21 -0500

    Initial commit

---
+++ src/Groovy_01_HelloWorld.groovy
@@ -0,0 +1,19 @@
+/*
+public class Groovy_01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+*/
+
+println "hello world"
+
+println this.class
+//this.run() # This will cause an infinite loop
+
+
+/* Output:
+
+hello world
+class Groovy_01_HelloWorld
+ */
