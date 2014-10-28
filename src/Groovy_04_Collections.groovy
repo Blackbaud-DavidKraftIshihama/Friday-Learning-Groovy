883038091d03717b394cafabcacd8de7e68e0b71
David Kraft-Ishihama <david.kraft-ishihama@blackbaud.com>
2014-10-28 16:27:10 -0500

    closures

--- src/Groovy_04_Collections.groovy
+++ src/Groovy_04_Collections.groovy
@@ -18,17 +18,26 @@ println c
 println c+[7, 8]
 println c-6
 
+def d = [1, 2, 3, 4, 5] as LinkedList
+println d.getClass()
+
+
+println 1..5
+println "${1..5}"
+
 // Maps
 def emptyMap = [:]
 println emptyMap.getClass()
 println emptyMap.size()
 
-def complexExpression() { return 20 }
-def map2 = [a: 5,
-        "b": 5,
-        6:"flip",
-        (complexExpression()):complexExpression()]
+def method() { return 20 }
+def map2 = [a        : 5,
+        "b"          : 5,
+        6            : "flip",
+        ( method() ) : method() ]
 println map2
-println map2.getClass()
 println map2[20]
 
+//BEWARE when interacting with Java generics:
+ArrayList<Integer> intList = ["27", new Object(), 56.788]
+println(intList)
---
+++ src/Groovy_05_Closures.groovy
@@ -0,0 +1,19 @@
+def closure = {a, b, c -> a + b + c}
+println closure(1, 2, 3)
+println closure.call(1, 2, 3)
+println closure.getMetaClass()
+println closure.class
+
+def closure2 = {String a -> println a}
+//closure2(new Object())
+closure2("hi")
+
+[1, 2, 3, 4, 5].each({a -> println a})
+def newList = [1, 2, 3, 4, 5].findAll({a -> a % 2 == 1})
+println newList
+
+def composition = {a, b -> a(b)}
+composition(closure2, "hi")
+
+println "${closure(1, 2, 3)}"
+println "${closure.class}"
